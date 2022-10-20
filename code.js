bd4cbf79f0310a8946892210ba66187cd3bbf0e9
mtyra321 <60244016+mtyra321@users.noreply.github.com>
2022-10-20 14:24:06 -0700

    table looks correct

--- code.js
+++ code.js
@@ -20,24 +20,16 @@ window.onload = function () {
 }
 function firstClick() {
     loadJSON(function (response) {
-        if (document.getElementById("answer1").childNodes.length == 1) {
+        if (document.getElementById("table1").rows.length < 2) {
             var users = JSON.parse(response).users;
-            console.log("json: " + JSON.stringify(users))
-            var table = document.createElement("table")
-            var titleRow = table.insertRow(0)
-            titleRow.insertCell(0).innerHTML = "id"
-            titleRow.insertCell(1).innerHTML = "First Name"
-            titleRow.insertCell(2).innerHTML = "Last Name"
-            titleRow.insertCell(3).innerHTML = "Email"
+            var table = document.getElementById("table1")
             for (i in users) {
-                console.log("i: " + i)
                 var row = table.insertRow()
                 row.insertCell(0).innerHTML = users[i].id
                 row.insertCell(1).innerHTML = users[i].firstName
                 row.insertCell(2).innerHTML = users[i].lastName
                 row.insertCell(3).innerHTML = users[i].email
             }
-            document.getElementById("answer1").appendChild(table)
         }
     });
 }
@@ -46,24 +38,16 @@ function firstClick() {
 
 function secondClick() {
     loadJSON(function (response) {
-        if (document.getElementById("answer2").childNodes.length == 1) {
+        if (document.getElementById("table2").rows.length < 2) {
             var orders = JSON.parse(response).orders;
-            console.log("json: " + JSON.stringify(orders))
-            var table = document.createElement("table")
-            var titleRow = table.insertRow(0)
-            titleRow.insertCell(0).innerHTML = "Price"
-            titleRow.insertCell(1).innerHTML = "Product"
-            titleRow.insertCell(2).innerHTML = "Status"
-            titleRow.insertCell(3).innerHTML = "User Id"
+            var table = document.getElementById("table2")
             for (i in orders) {
-                console.log("i: " + i)
                 var row = table.insertRow()
                 row.insertCell(0).innerHTML = orders[i].price
                 row.insertCell(1).innerHTML = orders[i].product
                 row.insertCell(2).innerHTML = orders[i].status
                 row.insertCell(3).innerHTML = orders[i].userId
             }
-            document.getElementById("answer2").appendChild(table)
         }
     });
 }
@@ -72,26 +56,16 @@ function secondClick() {
 
 function thirdClick() {
     loadJSON(function (response) {
-        if (document.getElementById("answer3").childNodes.length == 1) {
+        if (document.getElementById("table3").rows.length < 2) {
             var orders = JSON.parse(response).orders;
-            console.log("json: " + JSON.stringify(orders))
-            var table = document.createElement("table")
-            var titleRow = table.insertRow(0)
-            titleRow.insertCell(0).innerHTML = "Price"
-            titleRow.insertCell(1).innerHTML = "Product"
-            titleRow.insertCell(2).innerHTML = "Status"
-            titleRow.insertCell(3).innerHTML = "User Id"
+            var table = document.getElementById("table3")
             for (i in orders) {
-                if (orders[i].userId == 4) {
-                    console.log("i: " + i)
-                    var row = table.insertRow()
-                    row.insertCell(0).innerHTML = orders[i].price
-                    row.insertCell(1).innerHTML = orders[i].product
-                    row.insertCell(2).innerHTML = orders[i].status
-                    row.insertCell(3).innerHTML = orders[i].userId
-                }
+                var row = table.insertRow()
+                row.insertCell(0).innerHTML = orders[i].price
+                row.insertCell(1).innerHTML = orders[i].product
+                row.insertCell(2).innerHTML = orders[i].status
+                row.insertCell(3).innerHTML = orders[i].userId
             }
-            document.getElementById("answer3").appendChild(table)
         }
     });
 }
