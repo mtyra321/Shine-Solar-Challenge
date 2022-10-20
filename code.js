333389c6416652a1790061879281d3e91020dca6
mtyra321 <60244016+mtyra321@users.noreply.github.com>
2022-10-20 16:16:09 -0700

    added bearer token

--- code.js
+++ code.js
@@ -1,6 +1,24 @@
+
+
+ var customXMLHttpRequest = (function (jwtoken) {
+
+    function getXMLHttpRequest(method, url, async){
+        var xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.open(method, url, async);
+        xmlHttpRequest.setRequestHeader('Authorization', 'Bearer ' + jwtoken);
+        return xmlHttpRequest;
+    }
+
+    return getXMLHttpRequest;
+})(process.env.BearerToken);
+
+
+
+
+
 function loadJSON(callback) {
 
-    var xobj = new XMLHttpRequest();
+    var xobj = customXMLHttpRequest('post','https://mtyra321.github.io/Shine-Solar-Challenge/',true);
     xobj.overrideMimeType("application/json");
     xobj.open('GET', 'db.json', true);
     xobj.onreadystatechange = function () {
